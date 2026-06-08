6967ee1acf076e7e7a4d5eaeff4888fffc617563
Rafe-kiho <64768348+Rafe-kiho@users.noreply.github.com>
2026-06-08 11:06:14 +0900

    chore(release): bump macos app to v1.1.10

--- scripts/install_macos.sh
+++ scripts/install_macos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-VERSION="${WEDDING_LEDGER_VERSION:-v1.1.9}"
+VERSION="${WEDDING_LEDGER_VERSION:-v1.1.10}"
 REPO="Rafe-Giho/wedding-ledger"
 case "$VERSION" in
   v*) TAG="$VERSION" ;;
